2474056c8c52d95c17cd80101ae943aaca99ee71
Adam O'Brien <obrienadam89@gmail.com>
2016-03-29 14:41:01 -0400

    Finished the AdvectionTerm class functionality for vector fields.

--- src/FiniteVolume/Equation/AdvectionTerm.cpp
+++ src/FiniteVolume/Equation/AdvectionTerm.cpp
@@ -47,7 +47,51 @@ AdvectionTerm::AdvectionTerm(const VectorFiniteVolumeField &u, const VectorFinit
     :
       Term(var)
 {
+    const size_t nActiveCells = var.grid.nActiveCells();
 
+    for(const Cell& cell: var.grid.cells)
+    {
+        size_t rowX = cell.globalIndex();
+        size_t rowY = rowX + nActiveCells;
+
+        Scalar centralCoeff = 0.;
+
+        for(const InteriorLink &nb: cell.neighbours())
+        {
+            size_t colX = nb.cell().globalIndex();
+            size_t colY = colX + nActiveCells;
+
+            const Vector2D& uf = u.faces()[nb.face().id()];
+
+            Scalar coeff = std::min(dot(uf, nb.outwardNorm()), 0.);
+            centralCoeff += std::max(dot(uf, nb.outwardNorm()), 0.);
+
+            coefficients_.push_back(Triplet(rowX, colX, coeff));
+            coefficients_.push_back(Triplet(rowY, colY, coeff));
+        }
+
+        for(const BoundaryLink &bd: cell.boundaries())
+        {
+            Scalar src = dot(u.faces()[bd.face().id()], bd.outwardNorm());
+
+            switch(var.boundaryType(bd.face().id()))
+            {
+            case VectorFiniteVolumeField::FIXED:
+                sources_[rowX] -= src;
+                sources_[rowY] -= src;
+                break;
+
+            case VectorFiniteVolumeField::NORMAL_GRADIENT:
+                break;
+
+            default:
+                throw Exception("AdvectionTerm", "AdvectionTerm", "unrecognized or unspecified boundary type.");
+            }
+        }
+
+        coefficients_.push_back(Triplet(rowX, rowX, centralCoeff));
+        coefficients_.push_back(Triplet(rowY, rowY, centralCoeff));
+    }
 }
 
 //- External functions
